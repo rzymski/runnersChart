5a285599ae134d80a01b4ca00bcbf214169dfec4
rzymski <83671265+rzymski@users.noreply.github.com>
2023-09-22 08:27:53 +0200

    Słownik: key: dateTime value: list of numberOfLaps for every runner Wykres line.html nie dziala

--- runChart/views.py
+++ runChart/views.py
@@ -29,8 +29,31 @@ def bar_chart(request):
     return render(request, 'chart/bar.html', context)
 
 def line_chart(request):
-    runningLaps = RunningLap.objects.all()
+    dict = get_laps_for_every_time()
     context = {
-        "runningLaps": runningLaps
+        "dict": dict
     }
     return render(request, 'chart/line.html', context)
+
+
+import pytz
+def get_laps_for_every_time():
+    running_laps = list(RunningLap.objects.values_list('endLapDate', flat=True).distinct().order_by('endLapDate'))
+    europe_warsaw = pytz.timezone('Europe/Warsaw')
+    running_laps = [dt.astimezone(europe_warsaw) for dt in running_laps]
+    dict = {running_lap: [0] * Runner.objects.count() for running_lap in running_laps}
+    runners = Runner.objects.all()
+    for endLapDate in dict:
+        for runner in runners:
+            dict[endLapDate][runner.id-1] = get_max_laps_for_runner_before_date(runner.id, endLapDate)
+    return dict
+
+def get_max_laps_for_runner_before_date(runner_id, end_lap_date):
+    max_laps = RunningLap.objects.filter(
+        runnerId=runner_id,
+        endLapDate__lte=end_lap_date
+    ).order_by('-numberOfLaps').first()
+    if max_laps:
+        return max_laps.numberOfLaps
+    else:
+        return 0
